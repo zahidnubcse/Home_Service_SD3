569118e4b4d880163480572062264b5c3c93cccd
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2026-04-05 00:08:48 +0600

    update server

--- GB_Backend/server.js
+++ GB_Backend/server.js
@@ -6,33 +6,26 @@ import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
 import reviewRouter from "./routes/reviewRoute.js";
 import subscriberRoutes from "./routes/subscriberRoutes.js";
-import bookingRoutes from './routes/bookingRoutes.js';
- 
+import bookingRoutes from "./routes/bookingRoutes.js";
 
-
-// App Config
 const app = express();
-const port = process.env.PORT || 4000;
 
+// connect DB & cloudinary
 connectDB();
 connectCloudinary();
 
-// Middlewares
+// middleware
 app.use(express.json());
 app.use(cors());
 
-// API endpoints
+// routes
 app.use("/api/user", userRouter);
 app.use("/api/reviews", reviewRouter);
 app.use("/api/subscribers", subscriberRoutes);
 app.use("/api/bookings", bookingRoutes);
- 
 
 app.get("/", (req, res) => {
   res.send("API Working");
 });
 
-app.listen(port, () => console.log("Server started on PORT: " + port));
-
-
-
+export default app;
